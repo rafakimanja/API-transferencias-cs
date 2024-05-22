0a1e880aa46bdd6a2e2abc5ef4f7782ac414acb1
Rafael Lopes Ens <90486707+rafakimanja@users.noreply.github.com>
2024-05-22 20:45:52 -0300

    Delete Noticias do CS directory

--- Noticias do CS/API/serializer.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from API.models import Jogador
-
-class JogadorSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Jogador
-        fields = ['id', 'nome', 'origem', 'destino', 'movimentacao']
